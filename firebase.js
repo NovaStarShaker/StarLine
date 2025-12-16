b1c535370eca2fa60570d7163632a110840b016f
NovaStarShaker <7vc8s4kzz7@privaterelay.appleid.com>
2025-12-16 09:24:18 -0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,15 @@
 // firebase.js
-// This is included only in pages that need Firebase: feed.html, profile.html
-
-// Initialize Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDaZl3MtYPutJMY1aqozvVoN8WbjNnlW20",
   authDomain: "starline-social.firebaseapp.com",
   projectId: "starline-social",
+  storageBucket: "starline-social.appspot.com",
   messagingSenderId: "902689303596",
-  appId: "1:902689303596:web:b77d39e05d51c5b8993589"
+  appId: "1:902689303596:web:b77d39e05d51c5b8993589",
+  measurementId: "G-647YLJZ7BS"
 };
 
+// Initialize Firebase (compat version)
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
